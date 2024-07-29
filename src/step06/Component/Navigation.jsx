5009544b080bbeb101255ec9f4fd5ac6fd266952
pearl <kjj0837@naver.com>
2024-07-29 14:38:54 +0900

    step06. About스타일

---
+++ src/step06/Component/Navigation.jsx
@@ -0,0 +1,10 @@
+import "./Navigation.css";
+
+export function Navigation(){
+  return(
+    <div className="nav">
+      <a href="/">Home</a> <br/>
+      <a href="/about">About</a>
+    </div>
+  );
+}
